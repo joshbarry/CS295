1bf7ff72a9a2a5f304536f5b90aef0814ad53022
Tyler J. Sawyer <aayrlgrump@gmail.com>
2013-10-29 17:06:09 -0400

    Changed Error Box and Login Box Input / Output
    -Error Box title text changed
    -Login box input boxes were enlarged to stretch across the login frame.

--- content_web/error_box.php
+++ content_web/error_box.php
@@ -9,7 +9,7 @@
 if($errors != "") {
 	echo "<p>ERROR_001: Content Not found!</p>";
 } else if($errorMsg){
-	echo "<p><b>Login failed.</b></p><ul><b>\n";
+	echo "<p style='text-align:center;'><b>Authentication Failed!</b></p><ul><b>\n";
 	foreach($errorMsg as $err){
 		echo "<li style='color: #FF0000'>" . $err . "</li>\n";
 	}
--- content_web/login_box.php
+++ content_web/login_box.php
@@ -5,10 +5,10 @@
 		<h1 style="text-align:center;">Login</h1>
 			
 		<label for="txtUserName">Username:</label><br />
-		<input name="txtUserName" tabindex="210" type="text" size="30" id="txtUserName" <? echo "value='$userName'"; ?>/><br />
+		<input style="width:100%;" name="txtUserName" tabindex="210" type="text" size="30" id="txtUserName" <? echo "value='$userName'"; ?>/><br />
 	
 		<label for="txtPassWord">Password:</label><br />
-		<input name= "txtPassWord" tabindex="230" type="password" size="30" id="txtPassWord" <? echo "value='$passWord'"; ?> /><br />
+		<input style="width:100%;" name= "txtPassWord" tabindex="230" type="password" size="30" id="txtPassWord" <? echo "value='$passWord'"; ?> /><br />
 	
 		<input type="submit" tabindex="300" name="cmdAuth" value="Login" style="display:block; margin: 10px auto;" />
 	</fieldset>
