a3bdd6d11f1db7cf1f4758be199e6993863811ca
Tyler J. Sawyer <aayrlgrump@gmail.com>
2013-10-29 12:24:49 -0400

    Secured Login_Box
    Changed the post method for login_box to prevent entity injection.

--- content_web/login_box.php
+++ content_web/login_box.php
@@ -1,14 +1,14 @@
 <div id="login_frame">
 <h1 style="text-align:left;">Psych WebApp</h1>
-<form action="<? print $_SERVER['PHP_SELF']; ?>" method="post" >
+<form action="<? echo  htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" >
 	<fieldset>
 		<h1 style="text-align:center;">Login</h1>
 			
 		<label for="txtUserName">Username:</label><br />
-		<input name="txtUserName" tabindex="210" type="text" size="30" id="txtUserName" <? print "value='$userName'"; ?>/><br />
+		<input name="txtUserName" tabindex="210" type="text" size="30" id="txtUserName" <? echo "value='$userName'"; ?>/><br />
 	
 		<label for="txtPassWord">Password:</label><br />
-		<input name= "txtPassWord" tabindex="230" type="password" size="30" id="txtPassWord" <? print "value='$passWord'"; ?> /><br />
+		<input name= "txtPassWord" tabindex="230" type="password" size="30" id="txtPassWord" <? echo "value='$passWord'"; ?> /><br />
 	
 		<input type="submit" tabindex="300" name="cmdAuth" value="Login" style="display:block; margin: 10px auto;" />
 	</fieldset>
